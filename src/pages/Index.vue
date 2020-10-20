d46fa44a4aeffa5206a5de7f6b95ae20ac034853
kayode007 <kayodeibr@gmail.com>
2020-10-20 18:12:12 +0100

    Revert "master"
    This reverts commit 88478e72018d4a2d3ee7726ed8945371e919b132.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -667,6 +667,7 @@
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
   data() {
@@ -703,7 +704,7 @@ export default {
 
   methods: {
     loading_cars() {
-      this.axios
+      axios
         .get(
           "https://www.salvagebid.com/rest-api/v1.0/lots/search?page=1&per_page=26&type=car&make=*&model=*&search_id=&search_query=&year_from=2008&year_to=2021&sort_field=&sort_order=&sales_type=*&distance=*&destination_zip=&location_state=*&location_city=*&primary_damage=normal+wear+%26+tear&loss_type=*&title_name=*&exterior_color=*&odometer_min=*&odometer_max=*"
         )
@@ -714,7 +715,7 @@ export default {
 
     selected(id) {
       // console.log(id);
-      this.axios
+      axios
         .get("https://www.salvagebid.com/rest-api/v2/lots/" + id)
         .then(response => {
           this.view_selected = response.data.lot;
