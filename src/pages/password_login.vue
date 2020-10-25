7d70e0e8fbd789213402c19c5eaee0a93c2657cc
kayode007 <kayodeibr@gmail.com>
2020-10-25 21:07:50 +0100

    master

--- src/pages/password_login.vue
+++ src/pages/password_login.vue
@@ -4,7 +4,126 @@
       <sidebar />
     </div>
     <div class="col-12 col-md-7 q-pa-md">
-      
+      <div class="row">
+        <div class="col-12 col-md-6 q-pa-md">
+          <q-card class="my-card">
+            <q-card-section>
+              <div class="text-subtitle2">Change Email</div>
+            </q-card-section>
+            <q-card-section>
+              <q-input
+                rounded
+                class="q-pa-md"
+                outlined
+                v-model="form.current_email"
+                readonly
+                type="text"
+              />
+              <q-input
+                rounded
+                class="q-pa-md"
+                outlined
+                v-model="form.new_mail"
+                type="text"
+                label="New Email"
+                :rules="[val => (val && val.length > 0) || 'Your new email']"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="alternate_email" />
+                </template>
+              </q-input>
+              <q-input
+                rounded
+                class="q-pa-md"
+                outlined
+                v-model="form.password"
+                unelevated
+                :type="isPwd ? 'password' : 'text'"
+                label="New Password"
+                :rules="[
+                  val => (val && val.length > 0) || 'Your Password Please'
+                ]"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
+              <q-btn
+                color="primary full-width"
+                rounded
+                text-color="white"
+                label="change email"
+              />
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-12 col-md-6 q-pa-md">
+          <q-card class="my-card">
+            <q-card-section>
+              <div class="text-subtitle2">Change Password</div>
+            </q-card-section>
+            <q-card-section>
+              <q-input
+                rounded
+                class="q-pa-md"
+                outlined
+                v-model="form.current_password"
+                unelevated
+                :type="isPwd ? 'password' : 'text'"
+                label="current password"
+                :rules="[
+                  val => (val && val.length > 0) || 'Your current password'
+                ]"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
+              <q-input
+                rounded
+                class="q-pa-md"
+                outlined
+                v-model="form.new_password"
+                unelevated
+                :type="isPwd ? 'password' : 'text'"
+                label="New Password"
+                :rules="[val => (val && val.length > 0) || 'Your new password']"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
+              <q-btn
+                color="primary full-width"
+                rounded
+                text-color="white"
+                label="change password"
+              />
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +136,10 @@ export default {
   },
   data() {
     return {
-      
+      isPwd: true,
+      form: {
+        current_email: this.$auth.user().email
+      }
     };
   }
 };
