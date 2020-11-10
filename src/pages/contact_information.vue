3480a3943580860e757e5f70d33c3bd9fea708f1
Michael <67415283+thri60@users.noreply.github.com>
2020-11-10 16:24:09 +0100

    master

--- src/pages/contact_information.vue
+++ src/pages/contact_information.vue
@@ -11,21 +11,21 @@
             <div class="row">
               <div class="col-12 col-md-4 q-pa-md">
                 <p class="text-bold text-grey-9">Account Information</p>
-                <q-input dense rounded standout="bg-primary text-white text-bold" class="q-pa-sm" outlined v-model="physical.name" :label="this.$auth.user().full_name" hint="Full Name" disable />
+                <q-input dense rounded standout="bg-primary text-white text-bold" class="q-pa-sm" outlined v-model="name" :label="this.$auth.user().full_name" hint="Full Name" disable />
                 <br>
-                <q-input type="tel" dense rounded standout="bg-primary text-white" class="q-pa-sm" outlined v-model="physical.phone" label="Enter Your Phone Number" hint="Phone Number"   :rules="[val => (val && val.length > 0) || 'Phone Number is required']" />
+                <q-input type="tel" dense rounded  class="q-pa-sm" outlined v-model="phone" label="Enter Your Phone Number" hint="Phone Number"   :rules="[val => (val && val.length > 0) || 'Phone Number is required']" />
               </div>
               <div class="col-12 col-md-4 q-pa-md">
-                <p class="text-bold text-grey-9">Physical Address</p>
-                 <q-input dense rounded standout="bg-primary text-white" class="q-pa-sm" outlined v-model="physical.address" label="Enter Your Address" hint="Address" :rules="[val => (val && val.length > 0) || 'Address is required']" />
+                <p class="text-bold text-grey-9">Residential Address</p>
+                 <q-input dense rounded  class="q-pa-sm" outlined v-model="Residential.address" label="Enter Your Address" hint="Address" :rules="[val => (val && val.length > 0) || 'Address is required']" />
                    <br>
-                 <q-input dense rounded standout="bg-primary text-white" class="q-pa-sm" outlined v-model="physical.city" label="Enter Your City" hint="City" :rules="[val => (val && val.length > 0) || 'City is required']" />
+                 <q-input dense rounded  class="q-pa-sm" outlined v-model="Residential.city" label="Enter Your City" hint="City" :rules="[val => (val && val.length > 0) || 'City is required']" />
                    <br>
-                 <q-select dense rounded rounded standout="bg-primary text-white" class="q-pa-sm" outlined v-model="physical.state" :options="states" hint="State" label="State" />
+                 <q-select dense rounded rounded  class="q-pa-sm" outlined v-model="Residential.state" :options="states" hint="State" label="State" />
                  <br>
-                 <q-input dense rounded standout="bg-primary text-white" class="q-pa-sm" outlined v-model="physical.zipcode" label="Enter Your Zip Code" hint="Zip Code" :rules="[val => (val && val.length > 0) || 'Zip Code is required']" />
+                 <q-input dense rounded  class="q-pa-sm" outlined v-model="Residential.zipcode" label="Enter Your Zip Code" hint="Zip Code" :rules="[val => (val && val.length > 0) || 'Zip Code is required']" />
                  <br>
-                 <q-select dense rounded rounded standout="bg-primary text-white" class="q-pa-sm" outlined v-model="physical.country"  hint="Country" label=" Enter Your Country" disable />
+                 <q-select dense rounded rounded  class="q-pa-sm" outlined v-model="Residential.country"  hint="Country" label=" Enter Your Country" disable />
                  <br>
                     <q-toggle
                       v-model="toggle"
@@ -35,13 +35,18 @@
                       class="text-grey-9"
                       unchecked-icon="clear"
                     />
-                    <!-- <br>
-                     <q-btn rounded color="primary" text-color="white" label="Update Information" /> -->
-
               </div>
-              <div class="col-12 col-md-4 q-pa-md">
+              <div class="col-12 col-md-4 q-pa-md" v-if="!toggle">
                 <p class="text-bold text-grey-9">Mailing Address</p>
-                 <q-input dense rounded standout="bg-primary text-white" class="q-pa-sm" outlined v-model="text" label="Custom standout" />
+                 <q-input dense rounded  class="q-pa-sm" outlined v-model="mail.address" label="Enter Your Address" hint="Address" :rules="[val => (val && val.length > 0) || 'Address is required']" />
+                   <br>
+                 <q-input dense rounded  class="q-pa-sm" outlined v-model="mail.city" label="Enter Your City" hint="City" :rules="[val => (val && val.length > 0) || 'City is required']" />
+                   <br>
+                 <q-select dense rounded rounded  class="q-pa-sm" outlined v-model="mail.state" :options="mailing_states" hint="State" label="State" />
+                 <br>
+                 <q-input dense rounded  class="q-pa-sm " outlined v-model="mail.zipcode" label="Enter Your Zip Code" hint="Zip Code" :rules="[val => (val && val.length > 0) || 'Zip Code is required']" />
+                 <br>
+                 <q-select dense rounded rounded  class="q-pa-sm" outlined v-model="mail.country"  hint="Country" label=" Enter Your Country" disable />
               </div>
             </div>
           </q-card-section>
@@ -62,7 +67,16 @@ export default {
   },
   data() {
     return {
-      physical:{
+      name: "",
+      phone: "",
+      Residential:{
+        address:"",
+        city: "",
+        state: null,
+        zipcode: "",
+        country: "Nigeria"
+      },
+      mail:{
         name: "",
         phone: "",
         address:"",
@@ -72,6 +86,7 @@ export default {
         country: "Nigeria"
       },
         states: ["Abuja (FCT)", "Abia", "Adamawa", "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River", "Delta", "Ebonyi", "Edo", "Ekiti", "Enugu", "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara"],
+        mailing_states: ["Abuja (FCT)", "Abia", "Adamawa", "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River", "Delta", "Ebonyi", "Edo", "Ekiti", "Enugu", "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara"],
         toggle: false
     };
   }
